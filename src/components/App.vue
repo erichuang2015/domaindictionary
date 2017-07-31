051a319835cfd3b276fa7400660797c1f8eba1b4
Ben <vohzd@protonmail.com>
2017-07-31 22:44:04 +0100

    select element with tlds in it

---
+++ src/components/App.vue
@@ -0,0 +1,102 @@
+<template>
+	<div>
+		<router-view class="main-viewport"></router-view>
+	</div>
+</template>
+
+<script>
+	/*
+	 * libs
+	 */
+	import { mapGetters }					from "vuex";
+
+
+	export default {
+		name: "app",
+		computed: {
+			...mapGetters([
+			])
+		},
+		methods: {
+		}
+	}
+</script>
+
+<style >
+
+	html {
+		margin: 0;
+		padding: 0;
+	}
+
+	body {
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		font-family: 'Titillium Web', sans-serif;
+		color: #8c829b;
+		margin: 0;
+		padding: 0;
+		width: 100%;
+		height: 100%;
+		background: #221d2a;
+		font-size: 18px;
+	}
+
+	h1, h2, h3, h4, h5, h6, p, ul, li {
+		margin: 0;
+		padding: 0;
+		font-weight: normal;
+		margin-bottom: 16px;
+	}
+
+	a {
+		color: #9d7660;
+		text-decoration: none;
+	}
+
+	a:hover{
+		color: #82718e;
+	}
+
+	span {
+		width: 100%;
+		float: left;
+	}
+
+	h2 {
+		margin-bottom: 32px;
+		font-size: 22px;
+		font-weight: normal;
+	}
+
+	.main-viewport {
+		float: left;
+		padding: 32px;
+		width: 100%;
+	}
+
+
+	button, input {
+		outline: none;
+		background: rgba(85, 85, 96, 0.1);
+		border: none;
+		padding: 8px;
+		font-family: 'Titillium Web', sans-serif;
+		font-size: 18px;
+		color: #c8c8c8;
+		float: left;
+		width: calc(100% - 16px);
+		margin-bottom: 16px;
+	}
+
+	input[type="text"]{
+		width: calc(100% - 32px);
+
+	}
+
+	button:hover {
+		opacity: 0.8;
+		cursor: pointer;
+	}
+
+</style>
---
+++ src/components/Home/Home.vue
@@ -0,0 +1,57 @@
+<template lang="html">
+  <section class="float">
+    <section class="main-content-port">
+
+
+      <h2>Choose your Tld</h2>
+
+      <select v-model="selectedTld">
+        <option disabled value="">Please select one</option>
+        <option v-for="tld in allTlds" v-bind:value="tld.tld">{{ tld.tld }}</option>
+      </select>
+
+
+    </section>
+  </section>
+</template>
+
+<script>
+
+import { mapGetters }               from "vuex";
+
+import tlds   from "../../assets/tlds/tlds-parsed.csv";
+
+export default {
+  data(){
+    return {
+      allTlds: null,
+      selectedTld: "",
+    }
+  },
+  mounted(){
+    this.allTlds = tlds
+  },
+  watch: {
+    selectedTld(){
+      console.log(this.selectedTld);
+    }
+  }
+}
+</script>
+
+<style lang="css">
+
+  select {
+    float:left;
+    z-index:10;
+    width:  calc(100% - 64px);
+    font-size: 22px;
+    padding: 8px;
+    background: rgb(36, 35, 39);
+    border: none;
+    color: #81788e;
+    font-weight: normal;
+    outline: none;
+  }
+
+</style>
